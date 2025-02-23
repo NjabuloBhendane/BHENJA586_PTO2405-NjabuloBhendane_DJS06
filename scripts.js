f489c42ac999da48340a664c6db275a29c3bbf5c
Njabulo Bhendane <njabulobhendane1996@gmail.com>
2025-02-23 20:31:46 +0200

    Created a  filter function that negates provinces that include Cape and console logs the length of the provinces left

--- scripts.js
+++ scripts.js
@@ -1,20 +1,34 @@
 // A list of provinces:
-const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State']
+const provinces = [
+  "Western Cape",
+  "Gauteng",
+  "Northern Cape",
+  "Eastern Cape",
+  "KwaZulu-Natal",
+  "Free State",
+];
 
 // A list of names:
-const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie']
+const names = [
+  "Ashwin",
+  "Sibongile",
+  "Jan-Hendrik",
+  "Sifso",
+  "Shailen",
+  "Frikkie",
+];
 
 // A list of products with prices:
 const products = [
-  { product: 'banana', price: "2" },
-  { product: 'mango', price: 6 },
-  { product: 'potato', price: ' ' },
-  { product: 'avocado', price: "8" },
-  { product: 'coffee', price: 10 },
-  { product: 'tea', price: '' },
-]
+  { product: "banana", price: "2" },
+  { product: "mango", price: 6 },
+  { product: "potato", price: " " },
+  { product: "avocado", price: "8" },
+  { product: "coffee", price: 10 },
+  { product: "tea", price: "" },
+];
 
-// 1. ForEach 
+// 1. ForEach
 names.forEach((name, index) => {
   console.log(name);
   console.log(provinces[index]);
@@ -22,15 +36,18 @@ names.forEach((name, index) => {
 });
 
 // 2. Uppercase
-const uppercaseProvinces = provinces.map(province => province.toUpperCase());
+const uppercaseProvinces = provinces.map((province) => province.toUpperCase());
 console.log(uppercaseProvinces);
 
-
 // 3.Name Lengths
-const nameLengths = names.map(name => name.length);
+const nameLengths = names.map((name) => name.length);
 console.log(nameLengths);
 
-// 4. Sorting 
+// 4. Sorting
 
 const sortedProvinces = [...provinces].sort();
 console.log(sortedProvinces);
+
+//5. Filter Cape 
+const filteredProvinces = provinces.filter(province => !province.includes("Cape"));
+console.log(filteredProvinces.length);
